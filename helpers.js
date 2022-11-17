7f2f00823684db4cbe6d94abca8c3516ba24e838
John Livingston <jmlivingston@gmail.com>
2022-11-16 16:26:18 -0800

    Code cleanup

--- helpers.js
+++ helpers.js
@@ -1,4 +1,46 @@
 const { appInsightsClient } = require('./appInsights')
+const path = require('path')
+const fs = require('fs')
+
+const todos = JSON.parse(fs.readFileSync('./data/todos.json').toString())
+
+const getData = () => {
+  const dataPath = path.join(__dirname, 'data')
+  const files = fs.readdirSync(dataPath)
+  return files.reduce((acc, file) => {
+    const fileContents = fs.readFileSync(path.join(dataPath, file)).toString()
+    let json = {}
+    try {
+      json[path.basename(file).replace('.json', '')] = JSON.parse(fileContents)
+    } catch (error) {
+      console.log(`Invalid JSON format in ${path.join(dataPath, file)}`)
+    }
+    return {
+      ...acc,
+      ...json,
+    }
+  }, {})
+}
+
+const getId = (url) => {
+  const numberString = url?.split('/')?.[2]
+  return Number.parseInt(numberString)
+}
+
+const handleError = (request) => {
+  let error = null
+  const id = getId(request.url)
+  if (!isNaN(id)) {
+    const todo = todos.find((todo) => todo.id === id)
+    if (todo?.name?.includes(`${request.method} ERROR`)) {
+      error = {
+        status: 500,
+        message: `Unable to ${request.method} this todo!`,
+      }
+    }
+  }
+  return error
+}
 
 const logError = ({ error, request }) => {
   appInsightsClient.trackException({ exception: { error, request } })
@@ -8,4 +50,4 @@ const logRequest = ({ request, response }) => {
   appInsightsClient.trackRequest({ request, response })
 }
 
-module.exports = { logError, logRequest }
+module.exports = { getData, getId, handleError, logError, logRequest }
--- server.js
+++ server.js
@@ -2,59 +2,16 @@ const fs = require('fs')
 const jsonServer = require('json-server')
 const killable = require('killable')
 const nodeWatch = require('node-watch')
-const path = require('path')
-const { logError, logRequest } = require('./helpers')
-
-const todos = JSON.parse(fs.readFileSync('./data/todos.json').toString())
+const { getData, handleError, logError, logRequest } = require('./helpers')
 
 const watch = process.argv.includes('watch')
 const port = 4000
 let httpServer = null
 
-const getData = () => {
-  const dataPath = path.join(__dirname, 'data')
-  const files = fs.readdirSync(dataPath)
-  return files.reduce((acc, file) => {
-    const fileContents = fs.readFileSync(path.join(dataPath, file)).toString()
-    let json = {}
-    try {
-      json[path.basename(file).replace('.json', '')] = JSON.parse(fileContents)
-    } catch (error) {
-      console.log(`Invalid JSON format in ${path.join(dataPath, file)}`)
-    }
-    return {
-      ...acc,
-      ...json,
-    }
-  }, {})
-}
-
-const todoData = getData()
-
-const getId = (url) => {
-  const numberString = url?.split('/')?.[2]
-  return Number.parseInt(numberString)
-}
-
-const handleError = (request) => {
-  let error = null
-  const id = getId(request.url)
-  if (!isNaN(id)) {
-    const todo = todos.find((todo) => todo.id === id)
-    if (todo?.name?.includes(`${request.method} ERROR`)) {
-      error = {
-        status: 500,
-        message: `Unable to ${request.method} this todo!`,
-      }
-    }
-  }
-  return error
-}
-
 const startServer = () => {
   const server = jsonServer.create()
   const middlewares = jsonServer.defaults()
-  const router = jsonServer.router(todoData)
+  const router = jsonServer.router(getData())
   server.use(middlewares)
   server.use(jsonServer.bodyParser)
   server.use((request, response, next) => {
